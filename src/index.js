e43517b88b473a661eaacbbd2767a34f4220505b
rudraa19 <rudra19206@gmail.com>
2025-02-18 21:30:18 +0530

    feat: mvp structure

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import swagger from "@elysiajs/swagger";
-import { Elysia } from "elysia";
+import { Responses } from "@nexusog/golakost";
+import { Elysia, t } from "elysia";
+import { Account, Client, Users } from "node-appwrite";
+import { AuthRouter } from "./routes/auth";
+const client = new Client()
+  .setEndpoint(Bun.env.APPWRITE_ENDPOINT)
+  .setProject(Bun.env.APPWRITE_PROJECT_ID)
+  .setKey(Bun.env.APPWRITE_API_KEY);
 
 const app = new Elysia()
   .use(swagger())
-  .get("/", () => "Hello Elysia")
-  .listen(3000);
+  .group("/api", (app) => app.use(AuthRouter))
+  .listen(Bun.env.PORT);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
---
+++ src/routes/auth/index.js
@@ -0,0 +1,6 @@
+import Elysia from "elysia";
+import { CreateUserRouter } from "./create";
+
+export const AuthRouter = new Elysia({
+  prefix: "/auth",
+}).use(CreateUserRouter);
